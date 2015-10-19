f38957e2eb25278f4d622d8afc9236018d12ee75
Chris Chappell <chappc@rpi.edu>
2015-10-19 17:33:11 -0400

    Added some function to swatches

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
 
-$("#sliderRed").slider({
+$(".slider").slider({
     orientation: "horizontal",
     max: 255,
     min: 0
 });
+
+// make a random color and set it as background for swatch
+randcolor = Math.floor(Math.random() * 16777216).toString(16);
+randcolor = "#" + "0".repeat(6-randcolor.length) + randcolor;
+
+$("#gen")[0].style.backgroundColor = randcolor;
